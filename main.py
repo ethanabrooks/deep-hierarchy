c187cff7389ca2de73be6570f4f80ba9856a87e3
Ethan Brooks <ethanabrooks@gmail.com>
2019-10-25 11:48:49 -0400

    argparse.ArgumentDefaultsHelpFormatter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import argparse
+import itertools
 from pathlib import Path
 
 import torch
@@ -136,7 +137,7 @@ def main(
     network = Net().to(device)
     optimizer = optim.Adam(network.parameters(), lr=lr)
 
-    for epoch in range(1, epochs + 1):
+    for epoch in itertools.count():
         train(
             log_interval=log_interval,
             network=network,
@@ -154,41 +155,30 @@ def main(
 
 def cli():
     # Training settings
-    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument(
         "--batch-size",
         type=int,
         default=64,
         metavar="N",
-        help="input batch size for training (default: 64)",
+        help="input batch size for training",
     )
     parser.add_argument(
         "--test-batch-size",
         type=int,
         default=1000,
         metavar="N",
-        help="input batch size for testing (default: 1000)",
+        help="input batch size for testing",
     )
     parser.add_argument(
-        "--epochs",
-        type=int,
-        default=10,
-        metavar="N",
-        help="number of epochs to train (default: 10)",
-    )
-    parser.add_argument(
-        "--lr",
-        type=float,
-        default=0.01,
-        metavar="LR",
-        help="learning rate (default: 0.01)",
+        "--lr", type=float, default=0.01, metavar="LR", help="learning rate "
     )
     parser.add_argument(
         "--no-cuda", action="store_true", default=False, help="disables CUDA training"
     )
-    parser.add_argument(
-        "--seed", type=int, default=1, metavar="S", help="random seed (default: 1)"
-    )
+    parser.add_argument("--seed", type=int, default=0, metavar="S", help="random seed ")
     parser.add_argument(
         "--log-interval",
         type=int,
@@ -196,10 +186,10 @@ def cli():
         metavar="N",
         help="how many batches to wait before logging training status",
     )
-    parser.add_argument("--log-dir", default="/tmp/mnist", metavar="N")
+    parser.add_argument("--log-dir", default="/tmp/mnist", metavar="N", help="")
     four_rooms_parser = parser.add_argument_group("four_rooms_args")
     # four_rooms_parser.add_argument("--room-size", type=int, default=100)
-    four_rooms_parser.add_argument("--distance", type=float, default=100)
+    four_rooms_parser.add_argument("--distance", type=float, default=100, help="")
     main(**hierarchical_parse_args(parser))
 
 
