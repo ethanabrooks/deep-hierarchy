7bcbcb4e7b8ee78e216d88a6b16ab80be6a96b4c
Ethan Brooks <ethanabrooks@gmail.com>
2019-10-26 17:43:27 -0400

    got DeepHierarchical to run

--- network.py
+++ network.py
@@ -45,6 +45,7 @@ class DeepHierarchicalNet(ConvDeConvNet):
         **kwargs,
     ):
         super().__init__(**kwargs, hidden_size=hidden_size)
+        self.hidden_size = hidden_size
         self.max_depth = max_depth
         self.arity = arity
         self.task_splitter = nn.GRU(hidden_size, hidden_size, num_layers=num_gru_layers)
@@ -52,7 +53,10 @@ class DeepHierarchicalNet(ConvDeConvNet):
         self.embedding2 = nn.Sequential(
             nn.ReLU(True), nn.Linear(4 * self.embedding.embedding_dim, hidden_size)
         )
-        self.logits = nn.Linear(2 * hidden_size, 2)
+        self.logits = nn.Sequential(nn.ReLU(True), nn.Linear(2 * hidden_size, 2))
+        self.pre_decode = nn.Sequential(
+            nn.ReLU(True), nn.Linear(hidden_size, 8 * hidden_size), nn.ReLU(True)
+        )
 
     def decompose(self, task_matrix):
         for task in task_matrix:
@@ -64,11 +68,10 @@ class DeepHierarchicalNet(ConvDeConvNet):
         task = self.embedding(x).view(x.size(0), -1)  # type:torch.Tensor
         task = self.embedding2(task).unsqueeze(0)
         assert isinstance(task, torch.Tensor)
-        not_done = torch.ones(x.size(0), 1)
-        N = torch.ones(x.size(0))
+        not_done = torch.ones(x.size(0), 1, device=x.device)
 
         # TODO depth first
-        for _ in range(self.max_depth):
+        for i in range(self.max_depth):
             # TODO: cast goals back into goal-space
 
             # done
@@ -77,20 +80,19 @@ class DeepHierarchicalNet(ConvDeConvNet):
             one_hot = F.gumbel_softmax(self.logits(logits_input), hard=True)
             # TODO allow asymmetric tree
             _, not_done = torch.split(not_done * one_hot, 1, dim=-1)  # done stays done
-            N *= 2 ** not_done.squeeze(-1)
 
             # decompose
             done = 1 - not_done
-            subtasks = torch.cat(list(self.decompose(task)), dim=0)
-            task = torch.cat([task, task], dim=0)
+            subtasks = F.relu(torch.cat(list(self.decompose(task)), dim=0))
+            # relu ensures that padding is unique
+            task = F.pad(
+                task, [0, 0, 0, 0, 0, subtasks.size(0) - task.size(0)], value=-1
+            )
             task = done * task + not_done * subtasks
 
         # combine outputs
-        import ipdb
-
-        ipdb.set_trace()
-        output = torch.zeros(self.output_size)
-        # TODO: batch this
-        for g in task:
-            output += self.decode(g)
-        return torch.sigmoid(output)  # TODO: other kinds of combination
+        mask = (task != -1).any(dim=-1)
+        decoder_input = self.pre_decode(task[mask]).unsqueeze(-1).unsqueeze(-1)
+        decoded = self.decoder(decoder_input).squeeze(1)
+        padded = nn.utils.rnn.pad_sequence(torch.split(decoded, tuple(mask.sum(0))))
+        return padded.sum(0).sigmoid()  # TODO: other kinds of combination
