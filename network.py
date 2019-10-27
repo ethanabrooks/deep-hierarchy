40759677251128fba0e5eb4c238dc13abfa39853
Ethan Brooks <ethanabrooks@gmail.com>
2019-10-27 10:52:52 -0400

    copy body of forward()

--- network.py
+++ network.py
@@ -64,8 +64,10 @@ class DeepHierarchicalNet(DeConvNet):
             subtasks, _ = self.task_splitter(gru_input)
             yield subtasks
 
-    """
     def forward(self, x):
+        z = self.embedding(x).view(x.size(0), -1, 1, 1)
+        return self.decoder(z).squeeze(1)
+        """
         task = self.embedding(x).view(x.size(0), -1)  # type:torch.Tensor
         # task = self.embedding2(task).unsqueeze(0)
         assert isinstance(task, torch.Tensor)
